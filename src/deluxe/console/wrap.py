36a75a5911621c6f74534a8341a58bc9e6c55e1c
Gilles Coissac <gilles@ideographe.fr>
2024-08-16 08:05:36 +0200

    refactor: wrap.AnsiTextWrapper class
    breaks code into smaller parts
    add copyright notice about original authors

--- src/deluxe/console/wrap.py
+++ src/deluxe/console/wrap.py
@@ -13,20 +13,43 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with standard-deluxe. If not, see <https://www.gnu.org/licenses/>
+#
+#
+# The AnsiTextWrapper class in this module is based on code
+# from argparse-color-formatter project hosted here:
+# https://github.com/arrai-innovations/argparse-color-formatter
+#
+# This code was adapted to use our library plus some refactoring were done.
+# Copyright (c) 2017, Emergence by Design Inc.
+# Copyright (c) 2024, Arrai Innovations Inc.
 """Wrap module."""
 
 from __future__ import annotations
 
 from textwrap import TextWrapper
 
-from deluxe.console.ansi import strip_esc
+from deluxe.console.ansi import length, strip_esc
 
 
 class AnsiTextWrapper(TextWrapper):
     """TextWrapper SubClass aware of ansi escape codes."""
 
-    def _wrap_chunks(self, chunks: list[str]) -> list[str]:  # noqa: C901, PLR0912
-        # sourcery skip: low-code-quality
+    def _should_add_line(
+        self, lines: list[str], chunks: list[str], cur_len: int, width: int
+    ) -> bool:
+        return (
+            self.max_lines is None
+            or len(lines) + 1 < self.max_lines
+            or (
+                (
+                    not chunks
+                    or (self.drop_whitespace and len(chunks) == 1 and not chunks[0].strip())
+                )
+                and cur_len <= width
+            )
+        )
+
+    def _wrap_chunks(self, chunks: list[str]) -> list[str]:
         """Wrap a sequence of text chunks.
 
         Wrap a sequence of text chunks and return a list of lines of
@@ -63,16 +86,12 @@ class AnsiTextWrapper(TextWrapper):
         # from a stack of chucks.
         chunks.reverse()
 
-        while chunks:  # noqa: PLR1702
+        while chunks:
             # Start the list of chunks that will make up the current line.
             # cur_len is just the length of all the chunks in cur_line.
             cur_line: list[str] = []
             cur_len = 0
-
-            # Figure out which static string will prefix this line.
             indent = self.subsequent_indent if lines else self.initial_indent
-
-            # Maximum width for this line.
             width = self.width - len(indent)
 
             # First chunk on line is whitespace -- drop it, unless this
@@ -81,19 +100,17 @@ class AnsiTextWrapper(TextWrapper):
                 del chunks[-1]
 
             while chunks:
-                # modified upstream code, not going to refactor for ambiguous variable name.
-                _len = len(strip_esc(chunks[-1]))
+                _len = length(chunks[-1])
                 if cur_len + _len > width:
-                    # Nope, this line is full
                     break
                 cur_line.append(chunks.pop())
                 cur_len += _len
 
             # The current line is full, and the next chunk is too big to
             # fit on *any* line (not just this one).
-            if chunks and len(strip_esc(chunks[-1])) > width:
+            if chunks and length(chunks[-1]) > width:
                 self._handle_long_word(chunks, cur_line, cur_len, width)
-                cur_len = sum(map(len, cur_line))
+                cur_len = sum(map(length, cur_line))
 
             # If the last chunk on this line is all whitespace, drop it.
             if self.drop_whitespace and cur_line and not strip_esc(cur_line[-1]).strip():
@@ -101,39 +118,29 @@ class AnsiTextWrapper(TextWrapper):
                 del cur_line[-1]
 
             if cur_line:
-                if (
-                    self.max_lines is None  # noqa: PLR0916
-                    or len(lines) + 1 < self.max_lines
-                    or (
-                        (
-                            not chunks
-                            or (
-                                self.drop_whitespace and len(chunks) == 1 and not chunks[0].strip()
-                            )
-                        )
-                        and cur_len <= width
-                    )
-                ):
+                if self._should_add_line(lines, chunks, cur_len, width):
                     # Convert current line back to a string and store it in
                     # list of all lines (return value).
                     lines.append(indent + "".join(cur_line))
                 else:
-                    while cur_line:
-                        if (
-                            strip_esc(cur_line[-1]).strip()
-                            and cur_len + len(self.placeholder) <= width
-                        ):
-                            cur_line.append(self.placeholder)
-                            lines.append(indent + "".join(cur_line))
-                            break
-                        cur_len -= len(strip_esc(cur_line[-1]))
-                        del cur_line[-1]
-                    else:
-                        if lines:
-                            prev_line = lines[-1].rstrip()
-                            if len(strip_esc(prev_line)) + len(self.placeholder) <= self.width:
-                                lines[-1] = prev_line + self.placeholder
-                                break
-                        lines.append(indent + self.placeholder.lstrip())
+                    self._add_placeholder_line(lines, cur_line, cur_len, width, indent)
                     break
         return lines
+
+    def _add_placeholder_line(
+        self, lines: list[str], cur_line: list[str], cur_len: int, width: int, indent: str
+    ) -> None:
+        while cur_line:
+            if strip_esc(cur_line[-1]).strip() and cur_len + len(self.placeholder) <= width:
+                cur_line.append(self.placeholder)
+                lines.append(indent + "".join(cur_line))
+                return
+            cur_len -= length(cur_line[-1])
+            del cur_line[-1]
+        if lines:
+            prev_line = lines[-1].rstrip()
+            if length(prev_line) + len(self.placeholder) <= self.width:
+                lines[-1] = prev_line + self.placeholder
+                return
+        lines.append(indent + self.placeholder.lstrip())
+        return
