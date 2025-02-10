03e21c05b7eae228582d02f8adf4296be1700369
Gilles Coissac <gilles@ideographe.fr>
2025-02-10 17:15:46 +0100

    fix(process): adds async call
    - fix issue when passing env
    - integrates availabiilty module
    - writes docs string

--- src/deluxe/process.py
+++ src/deluxe/process.py
@@ -13,24 +13,36 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with standard-deluxe. If not, see <https://www.gnu.org/licenses/>
-"""Process module."""
+"""Process module.
+
+This module provides the Command class for working with system processes.
+"""
 
 from __future__ import annotations
 
-import getpass
+import asyncio
 import locale
+import os
 import pwd
 import re
 import shutil
 import subprocess
+from asyncio import Future, Task
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Literal, TypeAlias, overload
+
+from deluxe.availability import availability, supported
 
 
 if TYPE_CHECKING:
-    import os
+    from asyncio.subprocess import Process
+    from collections.abc import Mapping
 
 
+_USER_SUPPORT: bool = supported(only=("posix",), but=("wasi", "ios"))
+
+
+@availability(only=("posix",), but=("wasi", "ios"))
 def get_real_users() -> set[str]:
     """Return a set of all real user accounts on the system.
 
@@ -39,6 +51,8 @@ def get_real_users() -> set[str]:
     It excludes system accounts with a UID less than the minimum UID
     defined in the /etc/login.defs file.
 
+    Availability: Unix, not WASI, not iOS
+
     Returns:
         set[str]: A list of usernames for all real user accounts on the system.
     """
@@ -46,7 +60,7 @@ def get_real_users() -> set[str]:
         "r",
         encoding=locale.getpreferredencoding(False),  # noqa: FBT003
     ) as lgn:
-        min_uid = int(sch[1]) if (sch := re.search("^UID_MIN\\s+(\\d+)", lgn.read())) else 1000
+        min_uid = int(sch[1]) if (sch := re.search(r"^UID_MIN\s+(\d+)", lgn.read())) else 1000
     return {
         p.pw_name
         for p in pwd.getpwall()
@@ -58,21 +72,47 @@ def get_real_users() -> set[str]:
     }
 
 
-class Command:
-    """System command class."""
+StrOrBytesPath: TypeAlias = str | bytes | os.PathLike[str] | os.PathLike[bytes]
 
-    SYS_USERS: set[str] = get_real_users()
+
+class Command:
+    """System command class.
+
+    This class represents a system command and provides methods for running the command
+    and handling the output.
+
+    It also supports specifying the user to run the command as on POSIX systems.
+    The actual implementation use the sudo command to eecute the command
+    if user is specified.
+
+    Commands are never executed through a shell.
+
+    This class supports asynchronous execution of the subprocess using asyncio.
+
+    Args:
+        name (str): The name of the command.
+        path (os.PathLike[str] | None): The path to the command executable.
+        user (str | None): The user to run the command as
+
+    Raises:
+        Command.Error: If the command is not found on the system.
+        NotmplementedError: if user is specified on non POSIX systems.
+    """
+
+    _SYS_USERS: set[str] = get_real_users()
 
     class Error(Exception):
-        def __init__(self, msg: str, retcode: int = 0, cmd: list[str] | None = None) -> None:
+        def __init__(
+            self, msg: str | bytes | None, retcode: int = 0, cmd: tuple[str, ...] | None = None
+        ) -> None:
             if retcode and cmd:
-                _cmd = " ".join(cmd)
-                msg = f"command <{_cmd}> returned non-zero exit status {retcode}.\n{msg}"
-                self.returncode = retcode
-            self.msg = msg
+                cmd_ = " ".join(cmd)
+                msg = f"command <{cmd_}> returned non-zero exit status {retcode}.\n{msg or ''}"
+                self.returncode: int = retcode
+            self.msg: str = msg.decode() if isinstance(msg, bytes) else msg or ""
             super().__init__(msg)
 
-    __slots__ = ("_user", "command", "name")
+    __slots__: tuple[str, ...] = ("_user", "command", "name")
 
     def __init__(
         self,
@@ -83,51 +123,157 @@ class Command:
     ) -> None:
         if path and Path(path).is_file():
             command = str(path)
+
         if not (command := shutil.which(name)):
             msg = f"Command {path or name} not found on your system."
             raise Command.Error(msg)
 
         self.command: str = command
-        self._user: str = user or getpass.getuser()
         self.name: str = name
 
+        if not _USER_SUPPORT and user:
+            msg = "specifying user is only supported on POSIX plateforms."
+            raise NotImplementedError(msg)
+
+        self._user: str | None
+        self.user = user
+
     @property
-    def user(self) -> str:
+    def user(self) -> str | None:
         """Returns the user associated with this command."""
         return self._user
 
     @user.setter
-    def user(self, user: str) -> None:
-        if user not in Command.SYS_USERS or user != "root":
+    @availability(only=("posix",), but=("wasi", "ios"))
+    def user(self, user: str | None) -> None:
+        if user and (user not in Command._SYS_USERS or user != "root"):
             msg = f"User {user} not found on your system."
             raise Command.Error(msg)
         self._user = user
 
-    def _create_exception(self) -> type:
+    def _create_exception(self) -> type[Command.Error]:
         return type(f"{self.name.capitalize()}Error", (Command.Error,), {})
 
-    def _run(self, *args: str, user: str, capture: bool = True, **kwargs: Any) -> str:
-        """Run the command and return stdout when command completes successfully."""
-        _args = ["sudo", "-u", user]
+    def _compose(self, *args: str) -> tuple[str, ...]:
+        if self._user:
+            return ("sudo", "-u", self._user, "--preserve-env", self.command, *args)
+        return (self.command, *args)
 
-        if kwargs.get("env") is not None:
-            _env = ",".join(kwargs["env"].keys())
-            _args.append(f"--preserve-env={_env}")
+    @overload
+    def __call__(
+        self,
+        *args: str,
+        input: bytes | None = None,
+        capture: bool = True,
+        text: Literal[False],
+        encoding: None,
+        cwd: StrOrBytesPath | None = None,
+        env: Mapping[str, str] | None = None,
+    ) -> bytes: ...
 
-        _args.append(self.command)
-        _args.extend(args)
+    @overload
+    def __call__(
+        self,
+        *args: str,
+        input: str | None = None,
+        capture: bool = True,
+        text: Literal[True],
+        encoding: str = "UTF-8",
+        cwd: StrOrBytesPath | None = None,
+        env: Mapping[str, str] | None = None,
+    ) -> str: ...
+
+    def __call__(
+        self,
+        *args: str,
+        input: str | bytes | None = None,  # noqa: A002
+        capture: bool = True,
+        text: bool = True,
+        encoding: str | None = "UTF-8",
+        cwd: StrOrBytesPath | None = None,
+        env: Mapping[str, str] | None = None,
+    ) -> str | bytes:
+        """Run this command.
+
+        Args:
+            *args (str): The arguments to pass to the command.
+            input (str | bytes | None): The input to pass to the command.
+            capture (bool): Whether to capture the command output.
+            text (bool): Whether to return the output as text or bytes.
+            encoding (str | None): The encoding to use for text output.
+            cwd (StrOrBytesPath | None): The current working directory for the command.
+            env (Mapping[str, str] | None): The environment variables for the command.
+
+        Returns:
+            str | bytes: The output of the command when command completes successfully.
+
+        Raises:
+            Command.Error: If the command returns with a non-zero exit status.
+        """  # noqa: DOC502
+        args_ = self._compose(*args)
         cp = subprocess.run(  # noqa: S603
-            _args,
+            args_,
             capture_output=capture,
             shell=False,
             check=False,
-            encoding="UTF-8",
-            text=True,
-            **kwargs,
+            input=input,
+            text=text,
+            encoding=encoding if text else None,
+            cwd=cwd,
+            env=env,
         )
-        if cp.returncode:
-            raise self._create_exception()(cp.stderr, cp.returncode, _args)
-        return cp.stdout or ""  # noqa: DOC201
+        return cp.stdout or "" if text else b""
 
-    def __call__(self, *args: str, capture: bool = True, **kwargs: Any) -> str:  # noqa: D102
-        return self._run(*args, user=self._user, capture=capture, **kwargs)
+    async def async_call(
+        self,
+        *args: str,
+        input: bytes | None = None,  # noqa: A002
+        capture: bool = True,
+        cwd: StrOrBytesPath | None = None,
+        env: Mapping[str, str] | None = None,
+    ) -> Task[Future[bytes]]:
+        """Run this command asynchronously.
+
+        Args:
+            *args (str): The arguments to pass to the command.
+            input (bytes | None): The input to pass to the command.
+            capture (bool): Whether to capture the command output.
+            cwd (StrOrBytesPath | None): The current working directory for the command.
+            env (Mapping[str, str] | None): The environment variables for the command.
+
+        Returns:
+            asyncio.Task: A task representing the command execution.
+        """
+        args_ = self._compose(*args)
+
+        proc = await asyncio.subprocess.create_subprocess_exec(
+            *args_,
+            stdin=subprocess.PIPE if input else None,
+            stdout=subprocess.PIPE if capture else None,
+            stderr=subprocess.PIPE if capture else None,
+            cwd=cwd,
+            env=env,
+        )
+        loop = asyncio.get_running_loop()
+        future = loop.create_future()
+
+        async def _wait_co(
+            proc: Process, input_: bytes | None, future: Future[bytes]
+        ) -> Future[bytes]:
+            stdout, stderr = await proc.communicate(input_)
+            if proc.returncode and proc.returncode < 0:
+                future.set_exception(
+                    self._create_exception()(
+                        stderr,
+                        proc.returncode,
+                        args_,
+                    ),
+                )
+            else:
+                future.set_result(stdout or b"")
+            return future
+
+        return loop.create_task(
+            _wait_co(proc, input, future),
+            name=self.name,
+        )
